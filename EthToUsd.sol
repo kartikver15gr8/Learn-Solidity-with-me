bd39c1d078f1dd8f01ad7645a2b375e29be9f363
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2022-10-14 23:13:06 +0530

    Create EthToUsd.sol

---
+++ EthToUsd.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./SimpleStorageTwo.sol";
+
+contract ExtraStorage is SimpleStorageTwo {
+    function store(uint256 _favoriteNumber) public override {
+        favoriteNumber = _favoriteNumber + 5;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public override {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber + 10;
+    } 
+}
