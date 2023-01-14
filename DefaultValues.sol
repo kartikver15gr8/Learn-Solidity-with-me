5c603089f8acb9ff29bf2c7775e52783692fc71d
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2023-01-14 14:00:02 +0530

    Create DefaultValues.sol

---
+++ DefaultValues.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract DefaultValues {
+    bool public b; // false
+    uint256 public u; // 0
+    address public addr;  // 0x0000000000000000000000000000000000000000
+    string public str;  // null
+    bytes32 public b2; // 0x0000000000000000000000000000000000000000000000000000000000000000
+}
