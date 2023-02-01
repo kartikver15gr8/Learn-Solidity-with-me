ffaf6f498c514dfc2cfc8ac4f1d3b09ad8ada21f
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2023-02-01 14:18:50 +0530

    Create Inheritance.sol

---
+++ Inheritance.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+/* In this contract, I'll show you how we can inherit a contract
+   to another one. In this example we'll inhert contract "Alpha"
+   to the contract "Beta" */
+
+contract Alpha {
+    function alphaMinor() public pure virtual returns(string memory) {
+        return "Alpha Minor";
+    }
+
+    function alphaMajor() public pure virtual returns(string memory) {
+        return "Alpha Major";
+    }
+
+    function alphaSpecial() public pure virtual returns(string memory) {
+        return "Alpha Special";
+    }
+}
+
+
+contract Beta is Alpha {
+    function alphaMinor() public pure override returns(string memory) {
+        return "Beta Minor";
+    }
+
+    function alphaMajor() public pure virtual override returns(string memory) {
+        return "Beta Major";
+    }
+}
+
+// Multiple inheritance 
+
+contract Gamma is Beta {
+    function alphaMajor() public pure override returns(string memory) {
+        return "Gamma Rays turned Banner into Hulk!";
+    }
+}
