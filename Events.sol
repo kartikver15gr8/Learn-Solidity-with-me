ac424d1a125892c4e9c92089ecd7045893047255
Kartikey Verma <91731654+kartikver15gr8@users.noreply.github.com>
2023-02-01 13:47:49 +0530

    Update Events.sol

--- Events.sol
+++ Events.sol
@@ -3,13 +3,15 @@ pragma solidity ^0.8.0;
 
 contract Events {
     event Log(string message, uint256 val);
+    event IndexedLog(address indexed sender, uint256 val);
 
     function test() public {
         emit Log("Hello! this is a message.", 123);
     }
 
-    event Message(address indexed _from, string message);
+    event Message(address indexed _from, address indexed _to, string message);
+    
     function sendMessage(string calldata _message) public {
-        emit Message(msg.sender, _message);
+        emit Message(msg.sender, _to, _message);
     }
 }
